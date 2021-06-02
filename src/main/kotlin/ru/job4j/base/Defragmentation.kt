403de32af905f1d6876c3b80ef9f6a862e910c74
yor_email <daniil.stebnitsky@gmail.com>
2021-06-02 14:55:17 +0300

    6. Массивы и коллекции [189223#528720] изменения

--- src/main/kotlin/ru/job4j/base/Defragmentation.kt
+++ src/main/kotlin/ru/job4j/base/Defragmentation.kt
@@ -2,21 +2,19 @@ package ru.job4j.base
 
 fun defragment(array: Array<String?>) : Array<String?> {
     for (index in array.indices) {
-        if (array[index] == null) {
-            var i = index
-            while (array[index] == null && i < array.size) {
-                if (array[i] != null) {
-                    array[index] = array[i]
-                    array[i] = null
-                }
-                i++
+        var i = index
+        while (array[index] == null && i < array.size) {
+            if (array[i] != null) {
+                array[index] = array[i]
+                array[i] = null
             }
+            i++
         }
     }
     return array
 }
 fun main() {
-    val array : Array<String?> = arrayOf("#", null, null, null, "@", null, "&", "%")
+    val array : Array<String?> = arrayOf(null, "#", null, null, null, "@", null, "&", "%")
     defragment(array)
     for (string in array) {
         println(string)
