953ca54c10c18b02ab4122679c85596822cae23e
yor_email <daniil.stebnitsky@gmail.com>
2021-06-02 14:57:39 +0300

    4. Операторы условий if [189221#528718] изменения

--- src/main/kotlin/ru/job4j/base/MaxValue.kt
+++ src/main/kotlin/ru/job4j/base/MaxValue.kt
@@ -1,8 +1,10 @@
 package ru.job4j.base
 
+fun maxValue(first : Int, second : Int) : Int {
+    return if (first > second) first else second
+}
 fun maxValue(first: Int, second: Int, third: Int): Int {
-    return if (first > second && first > third) first
-    else if (second > third) second else third
+    return if (first > second) maxValue(first, third) else maxValue(second, third)
 }
 fun main() {
     println("Max value = " + maxValue(8, 7, 9))
